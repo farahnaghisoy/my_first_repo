99bd7082f08df86f370210086dd3e31288c18025
farahnaghisoy <ferehnagisoy7@gmail.com>
2025-01-15 08:02:48 +0400

    My first commit!

---
+++ To do list project/script2.js
@@ -0,0 +1,32 @@
+const element = document.querySelector("#clear-todos");
+
+// ---Element özellikleri---
+// console.log(element.id);
+// console.log(element.className);
+// console.log(element.classList);
+// console.log(element.classList[1]);
+// console.log(element.textContent);
+// console.log(element.innerHTML);
+// console.log(element.href);
+// console.log(element.style);
+// console.log(element);
+
+// ----Style ve Element özelliklerini değiştirme----
+// element.className = "btn btn-primary";
+// element.style.color = "yellow";
+// element.style.marginLeft =  "50px";
+// element.href = "https://www.udemy.com";
+// element.target = "_blank";
+// element.textContent = "Delete";
+// element.innerHTML = "<span style = 'color:green'>Delete</span>"
+// console.log(element);
+// const elements = document.querySelectorAll(".material-symbols-outlined"); //Node lists
+// elements.forEach(function(el){
+//     el.style.color = "orange";
+//     el.style.backraund = "#ffff";
+// })
+// console.log(elements);
+let value;
+
+const todo = document.querySelector(".todo-list");
+console.log(value);
